3ed962e96278a46ad424730e7666d0e5c556b639
Sandy Vanderbleek <sandy.vanderbleek@gmail.com>
2023-03-22 18:07:29 -0400

    Update pyplot_countsums.py

--- pyplot_countsums.py
+++ pyplot_countsums.py
@@ -18,11 +18,7 @@ def count_sums(a, t):
   return r(len(a) - 1, t)
 
 xs = numpy.linspace(10,210,num=20,dtype=int)
-nsq = [x ** 2 for x in xs]
-exp = [2 ** x for x in xs]
 stmt = 'count_sums([1]*{}, 1)'
 ys = [timeit.timeit(stmt.format(x), number=20, globals=globals()) for x in xs]
 matplotlib.pyplot.plot(xs, ys, 'or')
-matplotlib.pyplot.plot(xs, nsq, 'ob')
-matplotlib.pyplot.plot(xs, exp, 'og')
 matplotlib.pyplot.show()
